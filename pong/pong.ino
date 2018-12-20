f274e5551146ad311cc84647729517196d0d335a
Khue Quang Nguyen <khue.nguyen@optima.fi>
2018-12-20 16:37:35 +0200

    Pong's movements

--- pong/pong.ino
+++ pong/pong.ino
@@ -16,9 +16,9 @@ int gamestate = 0;
 int justpressed = 0;
 
 // Ball properties
-int ballx = TFT_HEIGHT / 2;
+int ballx = SWIDTH / 2;
 int balloldx = ballx;
-int bally = TFT_WIDTH / 2;
+int bally = SHEIGHT / 2;
 int balloldy = bally;
 int ballsize = 16;
 int ballright = 1;
@@ -27,10 +27,18 @@ int balldown = 1;
 // Paddle properties
 int paddlewidth = 8;
 int paddleheight = 64;
+
+// Player properties
 int playerx = 0;
 int playeroldx = 0;
 int playery = 0;
 int playeroldy = 0;
+
+// Computer properties
+int computerx = SWIDTH - paddlewidth;
+int computeroldx = 0;
+int computery = 0;
+int computeroldy = 0;
 
 void setup() {
   GO.begin();
@@ -64,75 +72,29 @@ void loop() {
       break;
     case 1: // Gameplay screen
       // Draw the ball
-      drawBall(ballx, bally, balloldx, balloldy);
-
-      // Remember last position
-      balloldx = ballx;
-      balloldy = bally;
+      drawBall();
       
-      // Move the ball right
-      if (ballright == 1) {
-        ballx = ballx + 1;
-      }
+      // Handle the ball movements
+      handleBallMovements();
 
-      // Move the ball left
-      if (ballright == -1) {
-        ballx = ballx - 1;
-      }
+      // Draw the player
+      drawPlayer();
 
-      // Reflect the ball off of the left side of the screen
-      if (ballx == 0) {
-        ballright = 1;
-      }
-
-      // Reflect the ball off of the right side of the screen
-      if (ballx + ballsize == SWIDTH - 1) {
-        ballright = -1;
-      }
-
-      // Move the ball down
-      if(balldown == 1) {
-        bally = bally + 1;
-      }
+      // Draw the computer
+      drawComputer();
       
-      // Move the ball up
-      if(balldown == -1) {
-        bally = bally - 1;
-      }
-      
-      // Reflect the ball off of the top of the screen
-      if(bally == 0) {
-        balldown = 1;
-      }
-      
-      // Reflect the ball off of the bottom of the screen
-      if(bally + ballsize == SHEIGHT - 1) {
-        balldown = -1;
-      }
+      // Handle player control
+      handlePlayerControls();
 
-      // Draw the player's paddle
-      drawPaddle(playerx, playery, playeroldx, playeroldy);
-
-      // Remember last position
-      playeroldx = playerx;
-      playeroldy = playery;
-      
-      // If the player presses Up and the paddle is not touching the top of the screen, move the paddle up
-      if(GO.JOY_Y.isAxisPressed() == 2 and playery > 0) {
-        playery = playery - 1;
-      }
-      
-      // If the player presses down and the paddle is not touching the bottom of the screen, move the paddle down
-      if(GO.JOY_Y.isAxisPressed() == 1 and playery + paddleheight < SHEIGHT) {
-        playery = playery + 1;
-      }
+      // Handle computer movements
+      handleComputerMovements();
 
       // Change the game state
       if (GO.BtnA.isPressed() and justpressed == 0) {
         justpressed = 1;
         gamestate = 2;
+        GO.lcd.clear();
       }
-      
       break;
     case 2:
       // Win screen
@@ -167,12 +129,136 @@ void loop() {
   GO.update();
 }
 
-void drawBall(int x, int y, int old_x, int old_y) {
-  GO.lcd.fillRect(old_x, old_y, ballsize, ballsize, BGCOLOR); // First fill in the last position with background color
-  GO.lcd.fillRect(x, y, ballsize, ballsize, BALLCOLOR); // Then draw ball at the new position
+/**
+ * Draw the ball
+ */
+void drawBall() {
+  // Draw lines with background color to replace old ball positions
+  // ballsize + 1 eliminates the extra pixel at the down-right corner of the ball when it moves diagonally up 
+  if (ballx > balloldx) {
+    GO.lcd.drawFastVLine(balloldx, balloldy, ballsize + 1, BGCOLOR);
+  } else {
+    GO.lcd.drawFastVLine(balloldx + ballsize, balloldy, ballsize + 1, BGCOLOR);
+  }
+
+  if (bally > balloldy) {
+    GO.lcd.drawFastHLine(balloldx, balloldy, ballsize + 1, BGCOLOR);
+  } else {
+    GO.lcd.drawFastHLine(balloldx, balloldy + ballsize, ballsize + 1, BGCOLOR);
+  }
+  
+  GO.lcd.fillRect(ballx, bally, ballsize, ballsize, BALLCOLOR); // Then draw ball at the new position
+  
+  // Remember last position
+  balloldx = ballx;
+  balloldy = bally;
 }
 
-void drawPaddle(int x, int y, int old_x, int old_y) {
-  GO.lcd.fillRect(old_x, old_y, paddlewidth, paddleheight, BGCOLOR);
-  GO.lcd.fillRect(x, y, paddlewidth, paddleheight, PADDLECOLOR);
+/**
+ * Draw the player
+ */
+void drawPlayer() {
+  if (playery > playeroldy) {
+    GO.lcd.drawFastHLine(playeroldx, playeroldy, paddlewidth, BGCOLOR);
+  } else {
+    GO.lcd.drawFastHLine(playeroldx, playeroldy + paddleheight, paddlewidth, BGCOLOR);
+  }
+  
+  GO.lcd.fillRect(playerx, playery, paddlewidth, paddleheight, PADDLECOLOR);
+
+  // Remember last position
+  playeroldx = playerx;
+  playeroldy = playery;
+}
+
+/**
+ * Draw the computer
+ */
+void drawComputer() {
+  if (computery > computeroldy) {
+    GO.lcd.drawFastHLine(computeroldx, computeroldy, paddlewidth, BGCOLOR);
+  } else {
+    GO.lcd.drawFastHLine(computeroldx, computeroldy + paddleheight, paddlewidth, BGCOLOR);
+  }
+  
+  GO.lcd.fillRect(computerx, computery, paddlewidth, paddleheight, PADDLECOLOR);
+
+  // Remember last position
+  computeroldx = computerx;
+  computeroldy = computery;
+}
+
+/**
+ * Handle the ball movements
+ */
+void handleBallMovements() {
+  // Move the ball right
+  if (ballright == 1) {
+    ballx = ballx + 1;
+  }
+
+  // Move the ball left
+  if (ballright == -1) {
+    ballx = ballx - 1;
+  }
+
+  // Reflect the ball off of the left side of the screen
+  if (ballx == 0) {
+    ballright = 1;
+  }
+
+  // Reflect the ball off of the right side of the screen
+  if (ballx + ballsize == SWIDTH - 1) {
+    ballright = -1;
+  }
+
+  // Move the ball down
+  if(balldown == 1) {
+    bally = bally + 1;
+  }
+  
+  // Move the ball up
+  if(balldown == -1) {
+    bally = bally - 1;
+  }
+  
+  // Reflect the ball off of the top of the screen
+  if(bally == 0) {
+    balldown = 1;
+  }
+  
+  // Reflect the ball off of the bottom of the screen
+  if(bally + ballsize == SHEIGHT - 1) {
+    balldown = -1;
+  }
+}
+
+/**
+ * Handle player controls
+ */
+void handlePlayerControls() {
+  // If the player presses Up and the paddle is not touching the top of the screen, move the paddle up
+  if(GO.JOY_Y.isAxisPressed() == 2 and playery > 0) {
+    playery = playery - 1;
+  }
+  
+  // If the player presses down and the paddle is not touching the bottom of the screen, move the paddle down
+  if(GO.JOY_Y.isAxisPressed() == 1 and playery + paddleheight < SHEIGHT) {
+    playery = playery + 1;
+  }
+}
+
+/**
+ * Handle computer movements
+ */
+void handleComputerMovements() {
+  //If the ball is higher than the computer's paddle, move the computer's paddle up
+  if(bally < computery) {
+    computery = computery - 1;
+  }
+  
+  //If the bottom of the ball is lower than the bottom of the computer's paddle, move the comptuer's paddle down
+  if(bally + ballsize > computery + paddleheight) {
+    computery = computery + 1;
+  }
 }
